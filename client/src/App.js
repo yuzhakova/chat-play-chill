e462798eb25475b0f93a43956fc28961792065f3
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-01 18:42:04 -0700

    add router and confirm server is running

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
 
 function App() {
   return (
     <div>
       <h1>Chat Play Chill</h1>
+      {/* <Router>
+          <Switch>
+            <Route path="/" exact component={Join} />
+            <Route path="/chat" component={Chat} />
+          </Switch>
+        </Router> */}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const http = require ('http');
 
 const PORT = process.env.PORT || 5000
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server); //back-end socketio has many methods
+
+app.use (router); //call router as app middleware
+
 
 server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
