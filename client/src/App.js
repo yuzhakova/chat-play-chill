2cc4e61f89530349d1826a90ff77605cfde4346b
William Ting <williamdjting@gmail.com>
2020-09-01 21:48:14 -0700

    add capitalization on component file

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,10 @@ function App() {
   return (
     <div>
       <h1>Chat Play Chill</h1>
-
+      <h2>Homepage</h2>
+      {/* <Homepage /> */}
+      {/* <Connect4 />
+      <Chess /> */}
     </div>
   );
 }
