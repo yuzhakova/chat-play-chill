eeda76c65a589d4195f638ede97bcf419af9b6eb
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-01 18:02:16 -0700

    add necessary chat  components (empty for now)

---
+++ client/src/Components/InfoBar/InfoBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InfoBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InfoBar
---
+++ client/src/Components/TextContainer/TextContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TextContainer
