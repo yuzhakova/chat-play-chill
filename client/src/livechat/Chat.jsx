d810214f288b14f01225c4fa78a1823158633b08
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-03 09:48:53 -0700

    back-end now runs on heroku

--- client/src/livechat/Chat.jsx
+++ client/src/livechat/Chat.jsx
@@ -18,8 +18,8 @@ const Chat = ({ location }) => { //hook useEffect lets you use lifecycle methods
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
-  // const ENDPOINT = 'https://chat-play-chill.herokuapp.com/';
+  // const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://chat-play-chill.herokuapp.com/';
   
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
