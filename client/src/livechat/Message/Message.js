2810523589c13ae7265152031b5493e8e01fd92d
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-03 09:25:48 -0700

    add messages logic + confirm waking on both ends

--- client/src/livechat/Message/Message.js
+++ client/src/livechat/Message/Message.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const Message = () => {
+import './Message.css';
+
+import ReactEmoji from 'react-emoji';
+
+const Message = ({ message: { text, user }, name }) => {
+  let isSentByCurrentUser = false;
+
+  const trimmedName = name.trim().toLowerCase();
+
+  if(user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    isSentByCurrentUser
+      ? (
+        <div className="messageContainer justifyEnd">
+          <p className="sentText pr-10">{trimmedName}</p>
+          <div className="messageBox backgroundGreen">
+            <p className="messageText">{ReactEmoji.emojify(text)}</p>
+          </div>
+        </div>
+        )
+        : (
+          <div className="messageContainer justifyStart">
+            <div className="messageBox backgroundLight">
+              <p className="messageText">{ReactEmoji.emojify(text)}</p>
+            </div>
+            <p className="sentText pl-10 ">{user}</p>
+          </div>
+        )
+  );
 }
 
-export default Message
+export default Message;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const socketio = require ('socket.io');
 const http = require ('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
@@ -9,18 +10,21 @@ const PORT = process.env.PORT || 5000
 const router = require('./router');
 
 const app = express();
-
 const server = http.createServer(app);
 const io = socketio(server); //back-end socketio has many methods
+
+app.use(router);
+app.use(cors());
 
 io.on('connect', (socket) => { //this will be connecting to client-side socket
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if(error) return callback(error); //if there is an error, we exit the function with return statement
+    socket.join(user.room);
 
     //admin generated messages
     //emit from the backend to the front-end
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the Chess Room ${user.room}` });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the Chat Room ${user.room}` });
     //method to send message to everyone except that user
     socket.broadcast.to(user.room).emit(`message`, { user: 'admin', text: `${user.name} has joined.`});
     socket.join(user.room);
@@ -35,7 +39,7 @@ io.on('connect', (socket) => { //this will be connecting to client-side socket
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { uer: user.name, text: message});
+    io.to(user.room).emit('message', { user: user.name, text: message });
 
     callback();
   });
@@ -45,13 +49,11 @@ io.on('connect', (socket) => { //this will be connecting to client-side socket
     const user = removeUser(socket.id);
 
     if(user) {
-      io.to(user.room).emit('message', { user: 'admind, text:   ${user.name} has left.'})
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-
     }
   })
 });
 
-app.use (router);
 
 server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
