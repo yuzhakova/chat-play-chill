4bf373137129f6529ced17f7c18c389566957369
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-02 19:09:12 -0700

    fix warning by adding <tbody> to <table> and <tr>

--- client/src/chesspage/Chesspage.jsx
+++ client/src/chesspage/Chesspage.jsx
@@ -6,14 +6,15 @@ export default function Chess() {
       <div>
         <p>This is the Chesspage</p>
         <table>
-          <tr>
-            <td><img src="https://www.chess.com/bundles/web/images/web/board-puzzles.600ddf36.png" alt=""></img></td>
-            {/* this is where chess component is rendered */}
-            <td>This is where live chat component is placed</td>
-            {/* this is where live chat component is placed */}
-          </tr>
+          <tbody>
+            <tr>
+              <td><img src="https://www.chess.com/bundles/web/images/web/board-puzzles.600ddf36.png" alt=""></img></td>
+              {/* this is where chess component is rendered */}
+              <td>This is where live chat component is placed</td>
+              {/* this is where live chat component is placed */}
+            </tr>
+          </tbody> 
         </table>
-        
       </div>
     </div>
   );
--- client/src/homepage/Homepage.jsx
+++ client/src/homepage/Homepage.jsx
@@ -7,15 +7,17 @@ export default function Homepage() {
       <div>
         <p>This is the Homepage</p>
         <table>
-          <tr>
-            <td>
-              <img src="https://image.shutterstock.com/image-vector/vector-illustration-cartoon-house-isolated-260nw-298428176.jpg" alt=""></img>
-              <p>This is where the two buttons for navigating to /chess and /connect 4 are rendered for interaction</p>
-              {/* this is where the two buttons for navigating to /chess and /connect 4 are rendered for interaction */}
-            </td>
-            <td>This is where live chat component is placed</td>
-            {/* this is where live chat component is placed */}
-          </tr>
+          <tbody>
+            <tr>
+                <td>
+                  <img src="https://image.shutterstock.com/image-vector/vector-illustration-cartoon-house-isolated-260nw-298428176.jpg" alt=""></img>
+                  <p>This is where the two buttons for navigating to /chess and /connect 4 are rendered for interaction</p>
+                  {/* this is where the two buttons for navigating to /chess and /connect 4 are rendered for interaction */}
+                </td>
+                <td>This is where live chat component is placed</td>
+                {/* this is where live chat component is placed */}
+              </tr>
+            </tbody>
         </table>
       </div>
     
