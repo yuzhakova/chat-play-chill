d2f16e17f8a1b04801506a614e9f4144d721ca65
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-02 23:53:51 -0700

    debug back-end + add input on front end

--- client/src/livechat/Input/Input.js
+++ client/src/livechat/Input/Input.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Input = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import './Input.css';
 
-export default Input
+const Input = ({ message, setMessage, sendMessage }) => ( //message property - destructure input into the props
+  <form className="form">
+    <input
+    className="input"
+    type="text"
+    placeholder="Share your thoughts"
+    value={message}
+    onChange={(event) => setMessage(event.target.value)}
+    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+
+    />
+    <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+  </form>
+)
+  export default Input;
+
--- server/index.js
+++ server/index.js
@@ -4,29 +4,31 @@ const http = require ('http');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const router = require('./router');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server); //back-end socketio has many methods (io.on 'connection' & 'disconnect' should be used when clients are joining or leaving)
 
-io.on('connection', (socket) => { //this will be connecting to client-side socket
-  socket.on('join', ({ name, room }) => {
+const server = http.createServer(app);
+const io = socketio(server); //back-end socketio has many methods
+
+io.on('connect', (socket) => { //this will be connecting to client-side socket
+  socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if(error) return callback(error); //if there is an error, we exit the function with return statement
-    
+
     //admin generated messages
     //emit from the backend to the front-end
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the Chess Room ${user.room}` });
     //method to send message to everyone except that user
     socket.broadcast.to(user.room).emit(`message`, { user: 'admin', text: `${user.name} has joined.`});
-
     socket.join(user.room);
 
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
     callback();
-  });
+  })
 
   //user generated messages
   //expect the event (back-end expects event from the front-end, the emitting part is on the front-end)
@@ -38,12 +40,18 @@ io.on('connection', (socket) => { //this will be connecting to client-side socke
     callback();
   });
 
+  //this callback will be run after an event is emmited
   socket.on('disconnect', () => {
-    console.log('User disconnected!');
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'admind, text:   ${user.name} has left.'})
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
+    }
   })
 });
 
-app.use (router); //call router as app middleware
-
+app.use (router);
 
 server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
