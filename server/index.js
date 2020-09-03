bb365ddb81be562300e605baf3ded7047f167f20
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-02 22:17:58 -0700

    back-end: build helper functions to manage users

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const socketio = require ('socket.io');
 const http = require ('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
---
+++ server/users.js
@@ -0,0 +1,34 @@
+//helper functions to manage users => used to manage users on specific sockets in index.js file
+
+const express = require("express");
+
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  //all lower case all one word no spaces
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  //check if user name is taken in the same room
+  const existingUser = users.find((user) => user.room === room && user.name === name);
+  
+  if(existingUser) {return { error: 'Username is taken'}};
+  
+  const user = { id, name, room };
+
+  users.push(user);
+
+  return { user };
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if(index !== -1) return users.splice(index, 1)[0]
+}
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
