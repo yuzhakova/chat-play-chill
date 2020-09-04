bee959d92bf6efb2a0ed3be6dfc4986c2a9bee7e
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-03 20:42:39 -0700

    set up the start for useDrag hook from react-dnd

--- client/src/chesspage/Board/Piece.jsx
+++ client/src/chesspage/Board/Piece.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useDrag } from 'react-dnd'
 
 export default function Piece({ piece: { type, color } }) {
+  const [ , drag] = useDrag({
+    item: { type: 'piece', id: `${type}_${color}` },
+  })
+
   const pieceImg = require(`../assets/${type}_${color}.png`)
   return (
-    <div>
-      <img src={pieceImg} alt="" />
+    <div className="piece-container" ref={drag}>
+      <img src={pieceImg} alt="" className="piece" />
     </div>
   )
 }
