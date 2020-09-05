9310b935c4f2955c6ceafecca920ed1bd3269256
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-04 22:22:34 -0700

    test deploy to netlify

---
+++ client/build/precache-manifest.f5ee8a32fc770da8d070e7da7f2fa178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9c1f65d5d628b996be2933f4a30e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fe26246d5f92bd2319",
+    "url": "/static/css/main.6cc22cf0.chunk.css"
+  },
+  {
+    "revision": "824f74a60f3245051234",
+    "url": "/static/js/2.bd5f7ec7.chunk.js"
+  },
+  {
+    "revision": "541fd958c32415f9eaa3b9720838319c",
+    "url": "/static/js/2.bd5f7ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fe26246d5f92bd2319",
+    "url": "/static/js/main.2eb786fc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/static/media/q_w.3382a953.png"
+  }
+]);
