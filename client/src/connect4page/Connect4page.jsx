530658938755c82a79e38a5e3bc8421ca5424779
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-09-05 05:13:01 -0700

    split screen on connect4 page

--- client/src/connect4page/Connect4page.jsx
+++ client/src/connect4page/Connect4page.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import Connect4Grid from "./Connect4Grid/Connect4Grid.tsx"
+import Join from '../livechat/Join/Join';
 
 
 export default function Connect4() {
   return (
     <div>
-      <div>
-        <p>This is the Connect4page</p>
-        <Connect4Grid />
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <img src="https://images-na.ssl-images-amazon.com/images/I/51PFqUcGZNL._AC_SY400_.jpg" alt=""></img>
-              {/* this is where connect 4 component is rendered */}
-              </td>
-              <td>This is where live chat component is placed</td>
-              {/* this is where live chat component is placed */}
-            </tr>
-          </tbody>
-        </table>
+      <div className="container">
+
+        <div className="split-section" id="split-1">
+          <div className="split-box">
+            <div className="board-container">
+              <Connect4Grid />
+            </div>
+          </div>
+        </div>
+
+        <div className="split-section" id="split-2">
+          <div className="split-box">
+            <Join />
+            <a href="/chess" className="btn btn-outline-white">play chess</a>
+            <h1 className="split-heading">
+            </h1>
+          </div>
+        </div>
+
       </div>
     </div>
   );
